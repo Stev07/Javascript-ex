800a1e407b439f6a3c945057427a986adaae43ec
Steve Dossin <steve.dossin@gmail.com>
2018-11-05 10:30:33 +0100

    Calculator2

---
+++ 02-maths/01-calculator-one/script.js
@@ -0,0 +1,35 @@
+/* becode/javascript
+ *
+ * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+    // to get the value of an input: document.getElementById("element-id").value
+    let nb1 = document.querySelector('#op-one');
+    let nb2 = document.querySelector('#op-two');
+
+    document.getElementById("addition").addEventListener("click", function() {
+        alert(parseInt(nb1.value)+parseInt(nb2.value))
+    });
+
+    document.getElementById("substraction").addEventListener("click", function() {
+        alert(parseInt(nb1.value)-parseInt(nb2.value))
+
+    });
+
+    document.getElementById("multiplication").addEventListener("click", function() {
+        alert(parseInt(nb1.value)*parseInt(nb2.value))
+
+    });
+
+    document.getElementById("division").addEventListener("click", function() {
+        alert(parseInt(nb1.value)/parseInt(nb2.value))
+
+    });
+})();
