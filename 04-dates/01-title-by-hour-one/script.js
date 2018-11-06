53d46ecd88dd947dd8868828631d45036a65a6a7
Steve Dossin <steve.dossin@gmail.com>
2018-11-06 12:10:33 +0100

    date1

---
+++ 04-dates/01-title-by-hour-one/script.js
@@ -0,0 +1,29 @@
+/* becode/javascript
+ *
+ * /04-dates/01-title-by-hour-one/script.js - 4.1: texte en fonction de l'heure (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+
+    let hour = (new Date()).getHours();
+    let content = document.querySelector("#target");
+
+    
+
+    if (hour < 18) {
+    content = content.innerHTML ='Bonjour';
+    }
+        else{
+            content = 'Bonsoir'
+        }
+
+    
+})();
