b946226928b0d15b7890f855bcb778c6cfdb951b
Steve Dossin <steve.dossin@gmail.com>
2018-11-08 16:02:57 +0100

    DOM6

---
+++ 06-dom/06-mini-carousel/script.js
@@ -0,0 +1,35 @@
+/* becode/javascript
+ *
+ * /06-dom/06-mini-carousel/script.js - 6.6: mini carousel
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    var gallery= [
+        "../../_shared/img/bell.svg",
+        "../../_shared/img/clock.svg",
+        "../../_shared/img/compass.svg",
+        "../../_shared/img/lemon.svg",
+        "../../_shared/img/map.svg",
+    ];
+
+
+    let index = 0;
+
+    document.querySelector('#next').addEventListener('click', ()=>{
+        if(index==4){
+            index=0;
+        } else{
+            index++;
+        }
+        document.querySelector('img').src = gallery[index];
+    });
+        
+
+})();
