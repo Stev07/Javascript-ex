2752dd7a17051a89a1ade6f6c4a396c466cf53f8
Steve Dossin <steve.dossin@gmail.com>
2018-11-09 12:22:33 +0100

    DOM8

---
+++ 06-dom/08-generate-table-two/script.js
@@ -0,0 +1,31 @@
+/* becode/javascript
+ *
+ * /06-dom/08-generate-table-two/script.js - 6.8: génération d'un tableau (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    function createTable() {
+        let rows;
+        let table = document.createElement("table");
+      
+
+        for (let i=0; i < 10; i++) {
+          rows = table.insertRow();
+
+          for (let y=0; y <10; y++){
+            rows.insertCell().textContent = (i+1)*(y+1);
+          }
+        }
+        document.querySelector('#target').appendChild(table);
+      }
+
+      createTable();
+
+})();
